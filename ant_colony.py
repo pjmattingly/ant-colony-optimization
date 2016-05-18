1ad771f145133a87ed412082bd004f0b1593599c
pjmattingly <pmattingly@mail.csuchico.edu>
2016-05-17 17:40:45 -0700

    cleanup testing code in ant_colony

--- ant_colony.py
+++ ant_colony.py
@@ -453,18 +453,3 @@ class ant_colony:
 			ret.append(self.id_to_key[id])
 		
 		return ret
-
-# def distance(start, end):
-	# x_distance = abs(start[0] - end[0])
-	# y_distance = abs(start[1] - end[1])
-	
-	# #c = sqrt(a^2 + b^2)
-	# import math	
-	# return math.sqrt(pow(x_distance, 2) + pow(y_distance, 2))
-	
-# test = {0: (0, 7), 1: (3, 9), 2: (12, 4), 3: (14, 11), 4: (8, 11), 5: (15, 6), 6: (6, 15), 7: (15, 9), 8: (12, 10), 9: (10, 7)}
-
-# #colony = ant_colony(test, distance, iterations=4, ant_count=5)
-# colony = ant_colony(test, distance)
-# result = colony.mainloop()
-# print(result)
