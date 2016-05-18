d32a6240161b6d10b884f9a82010875312ad852c
pjmattingly <pmattingly@mail.csuchico.edu>
2016-05-17 17:38:16 -0700

    cleanup

--- next_plus_testing.py
+++
@@ -1,6 +0,0 @@
-import decimal
-
-print(decimal.Decimal(0.0).next_plus())
-print(type(decimal.Decimal(0.0).next_plus()))
-print(float(decimal.Decimal(0.0).next_plus()))
-print(type(float(decimal.Decimal(0.0).next_plus())))
